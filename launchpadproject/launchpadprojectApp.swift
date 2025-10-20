e6c870824f543c97ca0b8b605f7eadba3645ef3c
ayushkogta <ayushkogta2@gmail.com>
2025-10-20 14:25:56 -0400

    Initial Commit

---
+++ launchpadproject/launchpadprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  launchpadprojectApp.swift
+//  launchpadproject
+//
+//  Created by Ayush on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct launchpadprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
